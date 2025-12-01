f59a39a6e5088aa0570d6b9c8d44f7e5c1fc7ad5
danielb088 <danielbaridk@gmail.com>
2025-12-01 19:39:43 +0200

    not working

--- wet_detect.py
+++ wet_detect.py
@@ -1,5 +1,16 @@
 import cv2
+import numpy as np
 
-img = cv2.imread("image.jpg")
+img = cv2.imread("photos\WIN_20251127_19_27_09_Pro.jpg")
 
-h, w = img.shape[:2]
+columns = np.array_split(img, 3, axis=1)
+for i, col in enumerate(columns):
+    cv2.imwrite(f"col_{i}.jpg", col)
+
+rows = np.array_split(img, 6, axis=0)
+for i, row in enumerate(rows):
+    cv2.imwrite(f"row_{i}.jpg", row)
+
+small = cv2.resize(img, None, fx=0.4, fy=0.4, interpolation=cv2.INTER_AREA)
+cv2.imshow("image",small)
+cv2.waitKey(0)
