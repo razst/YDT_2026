a9d91dcd7a078633f32ad401a42c917e25f9ad6e
danielb088 <danielbaridk@gmail.com>
2026-01-01 17:42:42 +0200

    comit

--- wet_detect.py
+++ wet_detect.py
@@ -1,16 +1,18 @@
 import cv2
 import numpy as np
+# from matplotlib import pyplot as plt
 
-img = cv2.imread("photos\WIN_20251127_19_27_09_Pro.jpg")
+img = cv2.imread("photos\WIN_20251127_19_27_09_Pro.jpg", cv2.IMREAD_GRAYSCALE)
+print(img)
+# columns = np.array_split(img, 3, axis=1)
+# for i, col in enumerate(columns):
+#     cv2.imwrite(f"col_{i}.jpg", col)
 
-columns = np.array_split(img, 3, axis=1)
-for i, col in enumerate(columns):
-    cv2.imwrite(f"col_{i}.jpg", col)
-
-rows = np.array_split(img, 6, axis=0)
-for i, row in enumerate(rows):
-    cv2.imwrite(f"row_{i}.jpg", row)
+# rows = np.array_split(img, 6, axis=0)
+# for i, row in enumerate(rows):
+#     cv2.imwrite(f"row_{i}.jpg", row)
 
 small = cv2.resize(img, None, fx=0.4, fy=0.4, interpolation=cv2.INTER_AREA)
 cv2.imshow("image",small)
 cv2.waitKey(0)
+
