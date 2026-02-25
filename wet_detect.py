18beebd22c2ea743b1a4c9fcaa269d8419020f52
danielb088 <danielbaridk@gmail.com>
2026-02-25 08:47:28 +0200

    return rows

--- wet_detect.py
+++ wet_detect.py
@@ -1,8 +1,8 @@
 import cv2
-
-#DEVIDE INTO 3 PARTS
-
+import time
 import numpy as np
+
+wet_precent = 60 #TODO CAP LETTER
 # from matplotlib import pyplot as plt
 
 # img = cv2.imread("photos\WIN_20251127_19_27_09_Pro.jpg", cv2.IMREAD_GRAYSCALE)
@@ -23,26 +23,7 @@ import numpy as np
 
 #tile = [[[1,2,3],[1,2,3],[1,2,3]],[[4,5,6],[4,5,6],[4,5,6]],[[7,8,9],[7,8,9],[7,8,9]]]
 
-def divide_image(image_path, tile_height, tile_width):
-    img = cv2.imread(image_path)
-    if img is None:
-        print("image not found")
-        return []
-
-    # Convert to HSV once
-    hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    img_h, img_w = hsv_img.shape[:2] # Safely get height and width
-    
-    grid = [] # This will be our nested list
-    for y in range(0, img_h, tile_height):
-        row_list = [] # Start a new row
-        for x in range(0, img_w, tile_width):
-            tile = hsv_img[y:y+tile_height, x:x+tile_width]
-            row_list.append(tile)
-        grid.append(row_list) # Add the full row to the grid
-    
-    return grid
-
+#TODO comments 
 def is_tile_wet(tile, percent_threshold):
     # 'tile' is an HSV array. Channel 2 is 'Value' (Brightness).
     v_channel = tile[:, :, 2]
@@ -56,10 +37,9 @@ def is_tile_wet(tile, percent_threshold):
     actual_percent = (dark_pixels / total_pixels) * 100
 
     print(actual_percent)
-    return actual_percent >= percent_threshold
+    return actual_percent >= percent_threshold, actual_percent
 
-def divide_into_three_rows(image_path):
-    img = cv2.imread(image_path)
+def divide_into_three_rows(img):
     hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
     h, w = img.shape[:2]
@@ -73,28 +53,108 @@ def divide_into_three_rows(image_path):
     rows = [top_row, mid_row, bottom_row]
     return rows
 
-rows = divide_into_three_rows("photos\WIN_20251127_19_27_09_Pro_EDIT.jpg")
-img = cv2.imread("photos\WIN_20251127_19_27_09_Pro_EDIT.jpg")
-
-
-print(is_tile_wet(rows[1], 30))
 
 
 
+def main_image(path):
+    frame = cv2.imread(path)
+    rows = divide_into_three_rows(frame)
+    print(rows)
+    frame_h = frame.shape[0]
+    row_h = frame_h // 3
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    for i, row in enumerate(rows):
+        is_wet = is_tile_wet(row, wet_precent)
+        y_position = int((row_h * i) + (row_h // 2))
+        # print("Zone "+str(i)+", is_wet: "+str(is_wet)) 
+        cv2.putText(frame, "Zone "+str(i)+", is_wet: "+str(is_wet), (20, y_position), font, 1, (255, 0, 0), 2)
+        # cv2.putText(frame,"test",(10,10),font,10,4,(255,0,0))
+    cv2.imshow("wet", frame)
+    cv2.waitKey(0)
 
-lower_bound = np.array([0, 0, 0])
-upper_bound = np.array([180, 255, 152])
+def main_movie(path):
+    cap = cv2.VideoCapture(path)
 
-mask1 = cv2.inRange(rows[0], lower_bound, upper_bound)
-mask2 = cv2.inRange(rows[1], lower_bound, upper_bound)
-mask3 = cv2.inRange(rows[2], lower_bound, upper_bound)
+    if not cap.isOpened():
+        print("Error: Could not open video.")
+        return 
 
-combined = np.vstack((mask1, mask2, mask3))
+    prev_time = 0
+    fps_count=0
+    fps_sum=0
 
-resized_comb = cv2.resize(combined, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_AREA)
-resized_img = cv2.resize(img, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_AREA)
-cv2.imshow('comb', resized_comb)
-cv2.imshow('original', resized_img)
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            print("End of video or cannot read frame.")
+            break
+        frame = cv2.rotate(frame, cv2.ROTATE_90_CLOCKWISE)
+
+        current_time = time.time()
+
+        # FPS = 1 / time between frames
+        fps = 1 / (current_time - prev_time) if prev_time != 0 else 0
+        prev_time = current_time
+        fps_count +=1
+        fps_sum+=fps
+
+        # Resize once (0.5 is usually enough)
+        frame = cv2.resize(frame, None, fx=0.2, fy=0.5, interpolation=cv2.INTER_AREA)
+        rows = divide_into_three_rows(frame)
+        frame_h = frame.shape[0]
+        row_h = frame_h // 3
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        for i, row in enumerate(rows):
+            is_wet = is_tile_wet(row, wet_precent)
+            y_position = int((row_h * i) + (row_h // 2))
+            # print("Zone "+str(i)+", is_wet: "+str(is_wet)) 
+            cv2.putText(frame, "Zone "+str(i)+", is_wet: "+str(is_wet), (20, y_position), font, 1, (255, 0, 0), 2)
+            # cv2.putText(frame,"test",(10,10),font,10,4,(255,0,0))
+        cv2.imshow("wet", frame)
+
+        if cv2.waitKey(0) & 0xFF == ord('q'):
+            break
+
+def return_rows(path):
+    row_list = []
+    frame = cv2.imread(path)
+    rows = divide_into_three_rows(frame)
+
+    for row in rows:
+        is_wet, precent = is_tile_wet(row, wet_precent)
+        row_list.append((is_wet, precent))
+    
+    return row_list
+
+list = return_rows("photos/wet1.jpg")
+print(list)
+
+
+# rows = divide_into_three_rows("photos\WIN_20251127_19_27_09_Pro_EDIT.jpg")
+# img = cv2.imread("photos\WIN_20251127_19_27_09_Pro_EDIT.jpg")
+
+# print(is_tile_wet(rows[1], 30))
+
+
+
+
+# lower_bound = np.array([0, 0, 0])
+# upper_bound = np.array([180, 255, 152])
+
+# mask1 = cv2.inRange(rows[0], lower_bound, upper_bound)
+# mask2 = cv2.inRange(rows[1], lower_bound, upper_bound)
+# mask3 = cv2.inRange(rows[2], lower_bound, upper_bound)
+
+# combined = np.vstack((mask1, mask2, mask3))
+
+# resized_comb = cv2.resize(combined, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_AREA)
+# resized_img = cv2.resize(img, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_AREA)
+# cv2.imshow('comb', resized_comb)
+# cv2.imshow('original', resized_img)
+
+
+
+
 
 # print(is_tile_wet(rows[0], 60))
 
@@ -106,6 +166,3 @@ cv2.imshow('original', resized_img)
 # resized_img = cv2.resize(mask, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_AREA)
 
 # cv2.imshow('wet mask', resized_img)
-
-
-cv2.waitKey(0)
