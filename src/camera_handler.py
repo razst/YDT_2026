5c9a81cfc40b553f45b44c92bc069710c20e41cb
danielb088 <danielbaridk@gmail.com>
2026-03-17 17:37:03 +0200

    camera final

--- src/camera_handler.py
+++ src/camera_handler.py
@@ -1,12 +1,16 @@
 import cv2
 import threading
 import queue
+from logger_handler import logger
 
 class Camera:
-    def __init__(self, source, frame_queue): #TODO auto start = false
+    def __init__(self, source, frame_queue, autp_start = False):
         self.cap = cv2.VideoCapture(source)
         self.q = frame_queue
         self.stopped = False
+
+        if autp_start:
+            self.start()
 
     def update(self):
         """
@@ -21,8 +25,9 @@ class Camera:
                 break
 
             # Logic to prevent the queue from bloating if processing is slow
-            if self.q.full(): # TODO add logger
+            if self.q.full(): 
                 try:
+                    logger.info("Frame queue is full")
                     self.q.get_nowait() # Drop oldest frame
                 except queue.Empty:
                     pass
@@ -31,7 +36,7 @@ class Camera:
 
     def start(self):
         """Run the update method in a background thread."""
-        thread = threading.Thread(target=self.update, daemon=True) #TODO check if damon is necserarry
+        thread = threading.Thread(target=self.update, daemon=True) 
         thread.start()
         return self
 
