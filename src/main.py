72bff706d065062e0fc88f97776223e52ed09b27
Raz <raz_st@hotmail.com>
2026-04-13 18:13:14 +0300

    refactore

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from mavLink_handler import *
 from constants import *
 from camera_handler import *
 import queue
-from lines_detect import *
+from target_detect import *
 
 if __name__ == "__main__":
    
@@ -17,10 +17,10 @@ if __name__ == "__main__":
 
    # Connect the Camera
    try:
-      frame_queue = queue.Queue(maxsize=10)  # Buffer up to 10 frames
+      frame_queue = queue.Queue(maxsize=10)  # Buffer up to 10 frames TBD MAXSIZE in constants.py
       camera = Camera(CAMERA_IDX, frame_queue,True) 
    except Exception:
-      logger.error("Unable to connect to FC")
+      logger.error("Unable to connect to Camera")
    # wait 4 GUIDED mode
    mode = False
    while mode != True:
